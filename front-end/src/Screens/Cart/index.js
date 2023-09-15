da691c78e5d3c6abf792b395a2e885c9eab6e638
Thomas James <stackangular@gmail.com>
2023-09-16 00:40:16 +0530

    cart functionality

--- front-end/src/Screens/Cart/index.js
+++ front-end/src/Screens/Cart/index.js
@@ -2,7 +2,8 @@ import { Row, Col } from 'antd'
 import { useParams } from 'react-router-dom';
 import { useGetProductDetailsQuery } from '../../slices/productsApiSlice';
 import { addToCart } from '../../slices/cartSlice';
-import { Rate,Input,Button } from 'antd';
+import { Rate, Input, Button } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons'
 import { useState } from 'react';
 import './style.scss'
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,42 +11,63 @@ import Card from '../../Components/Card/card';
 
 const Cart = () => {
   const { id: productId } = useParams();
-  console.log(productId)
   const { data, isLoading, error } = useGetProductDetailsQuery(productId);
-
-  const [qty,setQty] = useState("");
+  const { cartItems } = useSelector((state) => state.cart);
+  const [qty, setQty] = useState("");
   const dispatch = useDispatch();
-  const addtoCartHandler=async()=>{
-    await dispatch(addToCart({...data,qty}))
+  const addtoCartHandler = async (e,id) => {
+  
+
+    const filteredItem= cartItems.filter((item)=> item._id === id);
+
+    await dispatch(addToCart({ ...filteredItem[0], qty:e.target.value }))
   }
 
   return (
-    <Row align="top" gutter={16}>
-      <Col span={10} >
-        <img src={data?.image} width={"100%"} style={{borderRadius:"10px"}}/>
-      </Col>
-      <Col span={8}>
-        <div>
-          <h1>{data?.brand}</h1>
-          <hr />
-        </div>
-        <div>
-          <Rate disabled defaultValue={Math.ceil(data?.rating)} /> {data?.numReviews} reviews
-          <hr />
-        </div>
-        <div>
-          <p>{data?.description}</p>
-        </div>
+    <>
+      <h1>Your Cart</h1>
+      <Row align="top" gutter={16}>
 
-      </Col>
-      <Col span={6}>
-        <p>price: ${data?.price} </p>
-        <p>status: {data?.countinStock>0?"In stock":"Out of stock"} </p>
-        Qty : <Input value={qty} onChange={(e)=>setQty(e.target?.value)} style={{width:"50px",marginRight:"10px"}}/>
-        <Button type="primary" onClick={addtoCartHandler} disabled={data?.countinStock<=0}>Add to cart</Button>
-      </Col>
+        <Col span={10} >
+          {
+            cartItems.map((item) => {
+              return (
+                <Row gutter={[16,24]}>
+                  <Col span={10}>
+                    <img src={item.image} width="100%" height={"100px"} />
+                  </Col>
+                  <Col span={10} align={"middle"}>
+                    <h3>{`${item.brand} ${item.name}`}</h3>
+                    <p>{`Price: $ ${item.price}`}</p>
+                    <label>Qty </label>
+                    <Input type="text"  onChange={(e)=>addtoCartHandler(e,item._id)} value={item.qty} style={{ width: "20%" }} />
+                  </Col>
+                  <Col span={4}>
+                    <Row style={{height:"100%"}} justify={"center"} align={"middle"}>
+                      <Col>
+                        <DeleteOutlined style={{ fontSize: '26px' }} />
+                      </Col>
 
-    </Row>
+                    </Row>
+                  </Col>
+                </Row>
+
+              )
+            })
+          }
+        </Col>
+        <Col span={8}>
+        </Col>
+        <Col span={6}>
+          <p>price: ${data?.price} </p>
+          <p>status: {data?.countinStock > 0 ? "In stock" : "Out of stock"} </p>
+          Qty : <Input value={qty} onChange={(e) => setQty(e.target?.value)} style={{ width: "50px", marginRight: "10px" }} />
+          <Button type="primary" onClick={addtoCartHandler} disabled={data?.countinStock <= 0}>Add to cart</Button>
+        </Col>
+
+      </Row>
+    </>
+
 
 
 
